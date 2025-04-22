52d37e222be70214d1f07d3e3959a25f36d9d290
Tasyaay03 <144881189+Tasyaay03@users.noreply.github.com>
2025-04-22 12:47:27 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function togglePlay() {
+  var video = document.querySelector('#videoperkenalan video');
+  var button = document.querySelector('.play-button');
+
+  if (video.paused) {
+    video.play();
+    button.innerHTML = '⏸';
+  } else {
+    video.pause();
+    button.innerHTML = '▶';
+  }
+}
